1b87701b7bb8f1a1ecf3ad4e7c4d5cdbef70b161
maaland <mariusmaaland@gmail.com>
2015-10-02 21:16:52 +0200

    Add strip_words functon

--- imdb.py
+++ imdb.py
@@ -13,13 +13,17 @@ class FileReader():
     def read_words(self, path):
         os.chdir(path)
         for file in glob.glob('1_1.txt'):
-            wordSet = set([word.lower() for line in open(file, encoding='utf-8') for word in line.split()])   #creates a set with unique, lowercase words from the file
+            wordSet = set([self.strip_words(word.lower()) for line in open(file, encoding='utf-8') for word in line.split()])   #creates a set with unique, lowercase words from the file
             for word in wordSet:
                 if word in self.posDict:                    #if the word is already in the dict, count up by one
                     self.posDict[word] = self.posDict[word] + 1
                 else:                                        #if the word is not already in the dict, add it, and add 1
                     self.posDict[word] = 1
 
+    def strip_words(self, str):                                #method that removes the characters in the table from the string
+        trans_table = dict.fromkeys(map(ord, '@#$/<>?!:;-_"'), None)
+        str = str.translate(trans_table)
+        return str
 
 
 fr = FileReader()
